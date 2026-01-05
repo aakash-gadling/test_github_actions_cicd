e0bd4200f95e2d1d240ed57d430c8e9727f491b3
AAKASH GADLING <its99573@gmail.com>
2026-01-05 12:55:01 +0530

    ECR corrected

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -10,7 +10,11 @@ variable "services" {
   default     = ["service-a", "service-b", "service-c"]
 }
 
+# output "repo_urls" {
+#   description = "Map of ECR repository URLs keyed by service name"
+#   value = { for k, v in aws_ecr_repository.repo : k => v.repository_url }
+# }
+
 output "repo_urls" {
-  description = "Map of ECR repository URLs keyed by service name"
-  value = { for k, v in aws_ecr_repository.repo : k => v.repository_url }
+  value = tolist(aws_ecr_repository.repo[*].repository_url)
 }
